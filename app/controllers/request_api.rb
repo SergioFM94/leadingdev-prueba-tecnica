415f94224ea82dab9a7a22d53d7d93f63a773ab0
Sergio Fernandez <75262614+SergioFM94@users.noreply.github.com>
2022-08-29 01:15:58 +0200

    Update request_api.rb

--- app/controllers/request_api.rb
+++ app/controllers/request_api.rb
@@ -1,13 +1,1 @@
-require 'rest-client'
-require 'json'
 
-    def getFrase
-        url = 'https://api.chucknorris.io/jokes/random'
-        response = RestClient.get url
-        result = JSON.parse response.to_str
-
-        value = result['value']
-        puts "#{value}"
-    end
-
-    getFrase
